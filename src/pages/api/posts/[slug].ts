bf9b7ffe07d1a2eb71c119a31fafa1118c5170cc
Boris Moreno <borismoreno@Boriss-MacBook-Pro.local>
2024-12-27 17:12:58 -0500

    first commit

---
+++ src/pages/api/posts/[slug].ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from "astro";
+import { getEntry } from "astro:content";
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request }) => {
+
+    const { slug } = params;
+
+    const post = await getEntry("blog", slug);
+
+    if (!post) {
+        return new Response(
+            JSON.stringify({ msg: `Post ${slug} not found` }),
+            {
+                status: 404,
+                headers: { "Content-Type": "application/json" }
+            });
+    }
+
+    return new Response(JSON.stringify(post),
+        {
+            status: 200, headers: { "Content-Type": "application/json" }
+        });
+};
+
+// export const getStaticPaths = async () => {
+//     return [
+//         { params: { slug: "first-post" } }
+//     ]
+// };
