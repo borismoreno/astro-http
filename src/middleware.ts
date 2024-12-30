aa5a897cd1b80514a229e368f709134f3a3cb813
Boris Moreno <borismoreno@Boriss-MacBook-Pro.local>
2024-12-29 20:37:28 -0500

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+
+    if (context.url.pathname === "/about") {
+        console.log(`Request for ${context.url.pathname}`);
+        return context.rewrite("/about/index.html");
+    }
+
+    return next();
+});
