663d8218b2a6b815d434f2212193a77faf2bde5a
Boris Moreno <borismoreno@Boriss-MacBook-Pro.local>
2024-12-29 22:42:49 -0500

    includes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
 
-    if (context.url.pathname === "/about") {
+    if (context.url.pathname.includes("about")) {
         console.log(`Request for ${context.url.pathname}`);
         return context.rewrite(new Request("/blog"));
     }
