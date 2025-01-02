7974d24fdd92c33e2279445e26efca7b69099332
Boris Moreno <borismoreno@Boriss-MacBook-Pro.local>
2025-01-02 00:42:37 -0500

    astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import awsAmplify from 'astro-aws-amplify';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), db()],
 
     adapter: awsAmplify(),
     output: 'server',
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+
+
+    await db.insert(Clients).values([
+        { id: 1, name: "Kasim", age: 25, isActive: true },
+        { id: 2, name: "Boris", age: 40, isActive: true },
+        { id: 3, name: "Pame", age: 38, isActive: true },
+        { id: 4, name: "Mate", age: 19, isActive: true },
+        { id: 5, name: "Criss", age: 32, isActive: false },
+    ]);
+
+    console.log('Seeding database...');
 }
